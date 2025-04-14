83d4f084e86f9f5a6cdb79f9c5b2fbe268f87f3a
Mateo <mateofeit@gmail.com>
2025-04-14 17:37:01 +0000

    my observation space code got wiped bc asus sucks ):

--- envs/LegoStateSpace/TwoDim/observation_space.py
+++ envs/LegoStateSpace/TwoDim/observation_space.py
@@ -1,1 +1,6 @@
 # wrapper for state_space, valid moves in action_space, and action_constraints basically
+class ObservationSpace(object):
+    def __init__(self, action: tuple, state_space):
+        self.action = action
+        self.state_space = state_space
+
