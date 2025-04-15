78cde118a1950cfa3d6347013feb46ee6a84219f
Matero952 <mateofeit@gmail.com>
2025-04-14 23:53:18 -0600

    problem file generation works! just need to add resetting/overwriting

--- pddls/LegoDomain2d.pddl
+++ pddls/LegoDomain2d.pddl
@@ -2,9 +2,11 @@
     (:requirements :strips :negative-preconditions :adl :derived-predicates)
     (:predicates
         (clear ?row ?col)
-        (supported ?row ?col)
+        ; (supported ?row ?col)
         (trapped ?row ?col)
+        (occupied ?row ?col)
     )
+
     (:action move
         :parameters (
             ?old_row
@@ -14,12 +16,17 @@
         )
         :precondition (and 
         (not (trapped ?old_row ?old_col))
+        (occupied ?old_row ?old_col)
         (clear ?new_row ?new_col)
-        (supported ?new_row ?new_col)
+    
+        ; (supported ?new_row ?new_col)
         )
         :effect (and 
         (not (clear ?new_row ?new_col))
+        (occupied ?new_row ?new_col)
         (clear ?old_row ?old_col)
+        (not (occupied ?old_row ?old_col))
+        (not (trapped ?new_row ?new_col))
         )
     )
 )
--- pddls/generate_problem_file.py
+++ pddls/generate_problem_file.py
@@ -2,11 +2,52 @@
 #TODO: think about if we should flatten here or no
 from seeding import *
 import numpy as np
+import re as re
 
-def generate_problem_file(write_file, full_config):
-    for start_cfg in full_config:
-        for end_cfg in full_config:
-            print(start_cfg, end_cfg)
+def generate_problem_file(out_file, seed):
+    problem = generate_full_config_dict(generate_full_config())[seed]
+    start, end = problem
+    init_block = generate_string(start)
+    goal_block = generate_string(end)
+    with open(out_file, "r") as f:
+        lines = f.readlines()
+        for idx, line in enumerate(lines):
+            if re.search("init", line, re.IGNORECASE):
+                lines.insert(idx + 1, "\n" + init_block + "\n")
+                break
+    with open(out_file, "w") as f:
+        f.writelines(lines)
+    with open(out_file, "r") as f:
+        lines = f.readlines()
+        for idx, line in enumerate(lines):
+            if re.search("goal", line, re.IGNORECASE):
+                lines.insert(idx + 1, "\n" + goal_block + "\n")
+                break
+    with open(out_file, "w") as f:
+        f.writelines(lines)
+    return start
+
+def generate_string(state):
+    state_str = ""
+    max_row_idx, max_col_idx = np.shape(state)
+    max_row_idx = max_row_idx - 1
+    max_col_idx = max_col_idx -1
+
+    for r_idx, row in enumerate(state):
+        for c_idx, col in enumerate(row):
+            if col == 0:
+                state_str += f" (clear r{r_idx} c{c_idx})\n"
+                state_str += f" (not (occupied r{r_idx} c{c_idx}))\n"
+            elif col == 1:
+                state_str += f" (occupied r{r_idx} c{c_idx})\n"
+                state_str += f" (not (clear r{r_idx} c{c_idx}))\n"
+                if r_idx == max_row_idx:
+                    continue
+                elif state[r_idx + 1][c_idx] == 1:
+                    state_str += f" (trapped r{r_idx} c{c_idx})\n"
+    return state_str
+generate_problem_file("C:/Users/mateo/Github/mateogym/pddls/LegoProblem2d.pddl", 1)
+# print(generate_string(generate_problem_file("hehe.txt", 1)))
 
 
 
