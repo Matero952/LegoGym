585d075b4e4d05f0811b5e8a34c836cf026845e6
Matero952 <mateofeit@gmail.com>
2025-04-15 14:49:18 -0600

    started implementing unified planning which will be very useful later when we want to like check paths

--- pddls/solve.py
+++ pddls/solve.py
@@ -1,14 +1,4 @@
-import up_fast_downward as fd
-import subprocess
-def solve_problem(domain_file, problem_file):
-    command = [
-    "./fast-downward.py",
-    domain_file,
-    problem_file,
-    "--search",
-    "astar(blind())"
-]
-    result = subprocess.run(command, capture_output=True, text=True)
-    print(result.stdout)
-
-solve_problem("C:/Users/mateo/Github/mateogym/pddls/LegoDomain2d.pddl", "C:/Users/mateo/Github/mateogym/pddls/LegoProblem2d.pddl")
+import unified_planning
+from unified_planning.shortcuts import *
+def solve(engine_name):
+    with OneshotPlanner(name='Fast Downward'):
