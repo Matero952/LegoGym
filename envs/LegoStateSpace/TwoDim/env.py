7b2dbe7167f667d02a9b6f7cc0cc205329d021ee
Matero952 <mateofeit@gmail.com>
2025-04-19 17:26:24 -0600

    env tested and works just a bit slow but it fckn works (:

--- envs/LegoStateSpace/TwoDim/env.py
+++ envs/LegoStateSpace/TwoDim/env.py
@@ -4,10 +4,14 @@
 # TODO Add pathing please <3
 # TODO Add functionality where the agent doesnt exactly choose the actual min and max of a block so you just override it to
 # whichever block that the coordinate position is pointing to.
+from pathlib import Path
+import sys
+pddls_directory = Path.cwd() / 'pddls'
+sys.path.append(str(pddls_directory))
 from typing import Optional
 import numpy as np
 from parse_action import *
-from pddls.seeding import *
+from seeding import *
 from action_mapping import *
 from get_reward import *
 from generate_classic_episode import *
@@ -16,12 +20,16 @@ from action_space import *
 import random
 class legoenv(object):
     def __init__(self, episode_seed, trunc_limit):
-        self.episode = generate_classic_episode(episode_seed)
+        self.episode = generate_classic_episode(episode_seed, trunc_limit)
         self.trunc_limit = trunc_limit
-        self.state = None
+        self.state = self.episode['start']['state']
         self.move_count = 0
         self.truncated = False
         self.terminated = False
+        if self.truncated:
+            print(f'EXPERIMENT TRUNCATED AT {self.move_count} MOVES')
+        if self.terminated:
+            print(f"EXPERIMENT COMPLETE AFTER {self.move_count} MOVES")
     
     def step(self, action_idx: int):
         action = generate_action_mapping()[action_idx]
@@ -50,84 +58,22 @@ class legoenv(object):
         self.episode = generate_classic_episode(rand_seed)
         return None
 
-
-    # def step(self, action):
-    #     assert self._space is not None
-    #     assert self._time_step is not None
-    #     start_idxs, end_idxs = parse_action(action)
-    #     start_row, start_col = start_idxs
-    #     end_row, end_col = end_idxs
-    #     self._space[start_row][start_col] = 0
-    #     self._space[end_row][end_col] = 1
-
-
-# class PlanarLegoEnvironment(object):
-#     def __init__(self):
-#         self._space = None
-#         self._space_constraints = None
-#         self._blocks = None
-#         self._time_step = None
-
-#     @property
-#     def space(self):
-#         return self._space
-
-#     @space.setter
-#     def space(self, value: np.ndarray):
-#         self._space = value
-#         # space is represented with a numpy nd array
-
-#     @property
-#     def space_constraints(self):
-#         return self._space_constraints
-
-#     @space_constraints.setter
-#     def space_constraints(self, value):
-#         self._space_constraints = value
-#         # space constraints are just an instance of the space constraints class
-
-#     @property
-#     def blocks(self):
-#         return self._blocks
-
-#     @blocks.setter
-#     def blocks(self, value):
-#         self._blocks = value
-
-#     @property
-#     def time_step(self):
-#         return self._time_step
-
-#     @time_step.setter
-#     def time_step(self, value):
-#         self._time_step = value
-
-#     def display_human_readable_space(self):
-#         assert self._space is not None
-#         assert self._space_constraints is not None
-#         assert self._blocks is not None
-#         return np.flipud(self._space)
-#         # space is usually upside down to humans so this makes it actually like human-readable
-
-#     def step(self, action):
-#         assert self._space is not None
-#         assert self._space_constraints is not None
-#         assert self._blocks is not None
-#         assert self._time_step is not None
-#         done = False
-
-#         # Action is like this its probably a good idea to comment this bc otherwise i will lowk forget.
-#         # [block unique identifier(integer), block to move (row_min, col_min), new placement (row_min_new, col_min_new)]
-
-#         pass
-
-#     def reset(self):
-#         self.time_step = 0
-#         pass
-
-#     def render(self):
-#         pass
-
-#     def close(self):
-#         pass
-
+if __name__ == "__main__":
+    experiment = legoenv(10, 200)
+    move0 = experiment.step(602)
+    move1 = experiment.step(493)
+    move2 = experiment.step(453)
+    move3 = experiment.step(88)
+    move4 = experiment.step(358)
+    move5 = experiment.step(218)
+    move6 = experiment.step(453)
+    move7 = experiment.step(333)
+    move8 = experiment.step(218)
+    move9 = experiment.step(88)
+    move10 = experiment.step(233)
+    move11 = experiment.step(203)
+    move12 = experiment.step(333)
+    move13 = experiment.step(113)
+    move14 = experiment.step(454)
+    move15 = experiment.step(118)
+    print(move15)
