ce8a2e06c587c7298be2f128afbd10862819d294
Matero952 <mateofeit@gmail.com>
2025-04-19 21:55:32 -0600

    started run_experiment

--- experiments/claude.py
+++ experiments/claude.py
@@ -1,8 +1,6 @@
-from envs.LegoStateSpace.TwoDim.parse_action import *
 import re as re
 import os
 import anthropic
-
 class ClaudeExperiment:
     def __init__(self, model, prompt_function):
         self.client = anthropic.Anthropic(api_key=os.getenv('CLAUDE'))
---
+++ experiments/run_experiment.py
@@ -0,0 +1,13 @@
+from claude import *
+from grok import *
+from prompts import *
+from pathlib import Path
+import sys
+pddls_directory = Path.cwd() / 'pddls'
+envs_directory = Path.cwd() / 'envs'
+sys.path.append(str(pddls_directory))
+from seeding import *
+from solve import *
+sys.path.append(str(envs_directory))
+from LegoStateSpace.TwoDim.parse_action import *
+
